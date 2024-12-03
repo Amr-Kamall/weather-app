fa43f295961780f308b1ef217a83640d4d59dfd3
amr kamal <amr442028@gmail.com>
2024-12-03 09:49:42 +0000

    update styles

--- App.js
+++ App.js
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
   },
   appContainer: {
     flex: 1,
-    // position: "relative",
+    position: "relative",
   },
   backgroundImage: {
     position: "absolute",
